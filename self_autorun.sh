478bad5f8cf575641ff30c9a3e13ad8c48c866ab
icloudsheep <1539159795@qq.com>
2026-07-30 13:04:39 +0800

    test: 新增测试训练配置和脚本

--- self_autorun.sh
+++ self_autorun.sh
@@ -3,12 +3,13 @@ set -e
 
 ROOT="$(cd "$(dirname "$0")" && pwd)"
 CONFIGS=(
-    ckks-AB-1-Skeleton.yaml
-    ckks-AB-1.yaml
-    ckks-AB-10-Skeleton.yaml
-    ckks-AB-10.yaml
-    ckks-AB-25-Skeleton.yaml
-    ckks-AB-25.yaml
+    loss-AB-1-Skeleton-NI.yaml
+    loss-AB-1-NI.yaml
+#    ckks-AB-1.yaml
+#    ckks-AB-10-Skeleton.yaml
+#    ckks-AB-10.yaml
+#    ckks-AB-25-Skeleton.yaml
+#    ckks-AB-25.yaml
 )
 
 for config in "${CONFIGS[@]}"; do
@@ -16,5 +17,12 @@ for config in "${CONFIGS[@]}"; do
     bash "$ROOT/run.sh" "$ROOT/configs/$config"
 done
 
+#bash evaluate.sh configs/ckks-AB-1-Skeleton.yaml "3B-AB-01-SKE-MMLU" mmlu
+#bash evaluate.sh configs/ckks-AB-1.yaml "3B-AB-01-MMLU" mmlu
+#bash evaluate.sh configs/ckks-AB-10-Skeleton.yaml "3B-AB-10-SKE-MMLU" mmlu
+#bash evaluate.sh configs/ckks-AB-10.yaml "3B-AB-10-MMLU" mmlu
+#bash evaluate.sh configs/ckks-AB-25-Skeleton.yaml "3B-AB-25-SKE-MMLU" mmlu
+#bash evaluate.sh configs/ckks-AB-25.yaml "3B-AB-25-MMLU" mmlu
+
 echo "[autorun] all done, shutting down"
-/usr/bin/shutdown -h now
+#/usr/bin/shutdown -h now
