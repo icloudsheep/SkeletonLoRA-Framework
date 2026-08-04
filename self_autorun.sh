83fd5709f2c9fe774a2f1e2628731e7f68c2f8c0
icloudsheep <1539159795@qq.com>
2026-08-04 17:10:24 +0800

    test: 提交测试结果和更新配置文件

--- self_autorun.sh
+++ self_autorun.sh
@@ -3,14 +3,10 @@ set -e
 
 ROOT="$(cd "$(dirname "$0")" && pwd)"
 CONFIGS=(
-    ckks-AB-0.yaml
-    ckks-AB-0-Skeleton.yaml
-    loss-AB-0-NI.yaml
-    loss-AB-0-Skeleton-NI.yaml
-    ckks-7b-AB-1.yaml
-    ckks-7b-AB-1-Skeleton.yaml
-    ckks-7b-AB-0.yaml
-    ckks-7b-AB-0-Skeleton.yaml
+    loss-7b-AB-0-NI.yaml
+    loss-7b-AB-0-Skeleton-NI.yaml
+    loss-7b-AB-1-NI.yaml
+    loss-7b-AB-1-Skeleton-NI.yaml
 )
 
 for config in "${CONFIGS[@]}"; do
@@ -18,8 +14,10 @@ for config in "${CONFIGS[@]}"; do
     bash "$ROOT/run.sh" "$ROOT/configs/$config"
 done
 
-#bash evaluate.sh configs/ckks-AB-1-Skeleton.yaml "3B-AB-01-SKE-MMLU" mmlu
-#bash evaluate.sh configs/ckks-AB-1.yaml "3B-AB-01-MMLU" mmlu
+#bash evaluate.sh configs/ckks-7b-AB-0-Skeleton.yaml "7B-AB-00-SKE-MMLU" mmlu
+#bash evaluate.sh configs/ckks-7b-AB-0.yaml "7B-AB-00-MMLU" mmlu
+#bash evaluate.sh configs/ckks-7b-AB-1-Skeleton.yaml "7B-AB-01-SKE-MMLU" mmlu
+#bash evaluate.sh configs/ckks-7b-AB-1.yaml "7B-AB-01-MMLU" mmlu
 #bash evaluate.sh configs/ckks-AB-10-Skeleton.yaml "3B-AB-10-SKE-MMLU" mmlu
 #bash evaluate.sh configs/ckks-AB-10.yaml "3B-AB-10-MMLU" mmlu
 #bash evaluate.sh configs/ckks-AB-25-Skeleton.yaml "3B-AB-25-SKE-MMLU" mmlu
